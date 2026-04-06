86636f321e9aa7188dd5f848b60b644f76839ccf
Ammar_Khaled <ammarkhaled2003@gmail.com>
2026-04-06 21:56:41 +0200

    customizing django user model by adding our system roles

--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    class Role(models.TextChoices):
+        PATIENT = 'PATIENT'
+        DOCTOR = 'DOCTOR'
+        RECEPTIONIST = 'RECEPTIONIST'
+        ADMIN = 'ADMIN'
+
+    role = models.CharField(
+        max_length=12,
+        choices=Role.choices,
+        default=Role.PATIENT,
+    )
