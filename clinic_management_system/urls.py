62ab5522780dd363444c2d8e55a069719860a86a
Ammar_Khaled <ammarkhaled2003@gmail.com>
2026-04-10 14:12:59 +0200

    refactoring and implementing a token refresh endpoint

--- clinic_management_system/urls.py
+++ clinic_management_system/urls.py
@@ -1,16 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers
 from users import views
 from rest_framework_simplejwt.views import TokenObtainPairView
 
-router = routers.DefaultRouter()
-router.register(r"users", views.UserViewSet)
-router.register(r"groups", views.GroupViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
-    path("", include(router.urls)),
-    path('api/token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path("", include("users.urls")),
 ]
