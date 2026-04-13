b9d4ff1d39d20f8df453a3e3788939291aa56f97
Ammar_Khaled <ammarkhaled2003@gmail.com>
2026-04-13 17:47:11 +0200

    quick fix

--- users/serializers.py
+++ users/serializers.py
@@ -6,7 +6,8 @@ from rest_framework import serializers
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ["url", "username", "email", "groups", "role", "first_name", "last_name", "is_active"]
+        fields = ["url", "username", "email", "groups", "role", "first_name", "last_name", "is_active", "password"]
+        extra_kwargs = {"password": {"write_only": True}}
 
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
